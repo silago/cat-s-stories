665c1e341d4986dd47be66c9119fc2beab9336a1
ShadowOfPain <shadowofpain@mail.ru>
2015-12-01 18:10:26 +0300

    fix kitten
    fix camera, add pause fow menu, change amination

---
+++ Menu.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public bool showMenu;
+	public int window;
+	public GameObject camera;
+	public moveCam _moveCam;
+	public GameObject music;
+
+
+	void Start () {
+		showMenu = false;
+		window = 0;
+		camera = GameObject.Find ("Main Camera");
+		_moveCam = camera.GetComponent<moveCam> ();
+	}
+	
+	void Update () {
+		if(Input.GetKeyUp ("escape") && !showMenu){ 
+			/*if (!showMenu){*/
+			Cursor.visible = true;
+			showMenu = true; 
+			window = 1; 
+			_moveCam.enabled = false;
+			Time.timeScale = 0;
+			/*{ 
+			else if (showMenu) {
+				Cursor.visible = false;
+				showMenu = false;
+				window = 0;
+			}*/
+		} 
+
+		else if (Input.GetKeyUp ("escape") && showMenu){
+			Cursor.visible = false;
+			showMenu = false;
+			window = 0;
+			_moveCam.enabled = true;
+			Time.timeScale = 1;
+		}
+	}
+
+	void OnGUI (){
+
+			if (window == 1) {
+				GUI.Box (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 80, 200, 220), "Меню");
+
+				if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2 - 40, 180, 30), "Новая игра")) {   
+					Application.LoadLevel (0);
+				}   
+				if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2, 180, 30), "Об игре")) {   
+					window = 2; 
+				}   
+				if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2 + 40, 180, 30), "Назад")) { 
+					window = 0;
+					showMenu = false;
+					Cursor.visible = false;
+					_moveCam.enabled = true;
+					//moveKitten.enabled = true;
+					Time.timeScale = 1;
+				}
+				if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2 + 80, 180, 30), "Выход"))   			
+					window = 3;
+			}
+
+			if (window == 2) {
+				GUI.Label (new Rect (Screen.width / 2 - 90, Screen.height / 2 - 0, 180, 40), "Инфа о разрабе");   
+				if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2 + 100, 180, 30), "назад")) {   
+					window = 1;   
+				}   
+			}
+
+			if (window == 3) {
+				GUI.Box (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 60, 200, 120), "Выход?"); 
+				if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2 - 20, 180, 30), "Да")) {   
+					Application.Quit (); //Выход из игры 
+				}   
+				if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 2 + 20, 180, 30), "Нет")) {   
+					window = 1;   
+				}   
+			}
+
+			useGUILayout = false;  
+		
+	}
+}
---
+++ movePlayer.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour {
+
+	private GameObject player;
+
+	public static float speed = 2;
+	public static float _speed;
+	public int rotation = 250;
+	public int jump = 3;
+
+	public static float x = 0.0f;
+
+	public AnimationClip idleAnimation;
+	public AnimationClip walkAnimation;
+	public AnimationClip runAnimation;
+	public AnimationClip jumpPoseAnimation;
+	public AnimationClip ItchingAnimation;	
+	public AnimationClip MeowAnimation;
+	//public AnimationClip IdleSitAnimation;
+	public AnimationClip Sit_downAnimation;
+	public AnimationClip Stand_upAnimation;
+	public AnimationClip sitAnimation;
+	public AudioSource meow;
+	public AudioSource run;
+	public Text _Text;
+
+	private bool isMoving = false;
+	private bool isSiting = false;
+	private bool isRuning = false;
+	private Animation _animation;
+
+	private bool onGroud = false;
+	/*private double lastJumpTime = -10.0;
+	private double jumpRepeatTime = 0.01;*/
+	// Use this for initialization
+	void Start () {
+		_animation = GetComponent<Animation>();
+		_speed = speed;
+		player = (GameObject)this.gameObject;
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Terrain") {
+			onGroud = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		if (col.gameObject.tag == "Terrain") {
+			onGroud = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		isMoving = false;
+		//isSiting = false;
+		//isRuning = false;
+		//_Text.text = "Welcome!";
+
+		if (Input.GetKey (KeyCode.W)) {
+			player.transform.position += player.transform.forward * speed * Time.deltaTime;
+			//run.Play();
+			//_animation.CrossFade (walkAnimation.name);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			//speed = _speed/2;
+			player.transform.position -= player.transform.forward * speed *Time.deltaTime;
+		}
+		/*if (Input.GetKeyUp (KeyCode.S)) {
+			speed = _speed * 2;
+		}*/
+		if (Input.GetKey (KeyCode.A)) {
+			player.transform.position -= player.transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			player.transform.position += player.transform.right * speed * Time.deltaTime;
+		}
+		if ((Input.GetKey (KeyCode.Space))/*&& (lastJumpTime + jumpRepeatTime < Time.time)*/) {
+			player.transform.position += player.transform.up * jump * Time.deltaTime;
+			//lastJumpTime = Time.time;
+		}
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			speed = _speed * 2;
+		}
+		if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			speed = _speed;
+		} 
+
+		if (Input.GetKey (KeyCode.R)) {
+			meow.Play ();
+		}
+
+
+		if ((Input.GetAxis ("Vertical") != 0.0f) && !Input.GetKey (KeyCode.Space)) {
+			isMoving = true;
+			/*if (!run.isPlaying)
+				run.Play();*/
+			if (speed == _speed * 2){				
+				isRuning = true;
+				_animation.CrossFade (runAnimation.name);
+			}
+			else {
+				isRuning = false;
+				_animation.CrossFade (walkAnimation.name);
+
+			}
+		} else if (Input.GetKey (KeyCode.C)) {
+			_animation.Play (ItchingAnimation.name);
+			_animation [ItchingAnimation.name].layer = 1;
+			_animation [ItchingAnimation.name].wrapMode = WrapMode.Once;
+		} else if (Input.GetKey (KeyCode.R)) {			
+			_animation.Play (MeowAnimation.name);
+			_animation [MeowAnimation.name].layer = 1;
+			_animation [MeowAnimation.name].wrapMode = WrapMode.Once;
+		} else if (Input.GetKeyUp (KeyCode.F)) {	
+			SittingKitten ();
+			/*_animation.Play (IdleSitAnimation.name);
+			_animation [IdleSitAnimation.name].layer = 1;
+			_animation [IdleSitAnimation.name].wrapMode = WrapMode.Loop;*/
+		} else/* if ((Input.GetKey (KeyCode.F)) && (isSiting)) { // если сидит
+			_animation.Stop (IdleSitAnimation.name);
+			_animation.CrossFade (idleAnimation.name);
+			isSiting = false; 
+		} else*/ if ((Input.GetKey (KeyCode.Space))&& onGroud == false) {
+			_animation.Play (jumpPoseAnimation.name);
+			_animation [jumpPoseAnimation.name].wrapMode = WrapMode.ClampForever;
+		} else if (onGroud == true) {
+			_animation.Stop (jumpPoseAnimation.name);
+			_animation.CrossFade (idleAnimation.name);
+		} else 
+			_animation.CrossFade (idleAnimation.name);
+	
+		/*if (isSiting){
+			Debug.Log ("isSiting");
+			if(Input.GetKey (KeyCode.F)){
+				isSiting = false;
+				_animation.Stop (IdleSitAnimation.name);
+			} else {
+				_animation.Play (IdleSitAnimation.name);
+				_animation [IdleSitAnimation.name].layer = 1;
+				_animation [IdleSitAnimation.name].wrapMode = WrapMode.Loop;
+			}
+		}*/
+
+		if (isMoving) {
+			/*if (isRuning)
+			{
+				run.pitch = 2.99f;
+				Debug.Log ("running");
+			}*/
+			if (!run.isPlaying){
+
+				run.Play();
+
+			}
+		}
+		else run.Stop();
+
+
+
+		Quaternion rotate = Quaternion.Euler (0, x, 0);
+		if (isMoving)
+		player.transform.rotation = rotate;
+	}
+
+
+	void SittingKitten () {
+		//Debug.Log (isSiting);
+		if (!isSiting) {
+			/*_animation.Play (IdleSitAnimation.name);
+			_animation [IdleSitAnimation.name].layer = 1;
+
+			_animation [IdleSitAnimation.name].wrapMode = WrapMode.Loop;*/
+
+			_animation.Play (Sit_downAnimation.name);
+			_animation [Sit_downAnimation.name].layer = 1;
+			_animation [Sit_downAnimation.name].wrapMode = WrapMode.Once;
+
+			_animation.Play (sitAnimation.name);
+			//_animation [sitAnimation.name].layer = 1;
+			_animation [sitAnimation.name].wrapMode = WrapMode.Loop;
+
+			isSiting= true;
+			return;
+		} else if (isSiting) {
+			_animation.Stop (sitAnimation.name);
+			_animation.Play (Stand_upAnimation.name);
+			_animation [Stand_upAnimation.name].wrapMode = WrapMode.Once;
+			isSiting = false;
+			return;
+		}
+		//return;
+	}
+
+
+}
